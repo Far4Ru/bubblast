6ffbfeb779766b064c885ef018d20a025edd8c4e
Far4Ru <faty422@mail.ru>
2024-07-25 11:27:58 +0300

    Changed game screen format

--- src/engine/window.cpp
+++ src/engine/window.cpp
@@ -5,7 +5,7 @@ Window::Window() {
 }
 
 int Window::create() {
-    this->window = SDL_CreateWindow(GAME_NAME, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+    this->window = SDL_CreateWindow(GAME_NAME, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_FULLSCREEN_DESKTOP);
     if (this->window == NULL) {
         SDL_Log(GAME_ERROR_WINDOW_CREATE);
         // SDL_Log(SDL_GetError());
