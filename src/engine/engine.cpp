f21657b5300ae384c48bb9f277c25fc69a593103
Far4Ru <faty422@mail.ru>
2024-07-25 11:28:21 +0300

    United surfacecode

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -5,11 +5,10 @@ Engine::Engine() {
     this->window = new Window();
     this->window->create();
 
+    /** surface */
     SDL_Surface* screen_surface = NULL;
-    
     screen_surface = SDL_GetWindowSurface(this->window->get());
-
-    SDL_FillRect(screen_surface, NULL, SDL_MapRGB( screen_surface->format, 255, 255, 255));
+    SDL_FillRect(screen_surface, NULL, SDL_MapRGB(screen_surface->format, 255, 255, 255));
 
     SDL_UpdateWindowSurface(this->window->get());
 
