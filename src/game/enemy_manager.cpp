deca34afb849c29ab5c31b6289295c7bfe199797
Far4Ru <faty422@mail.ru>
2024-08-18 22:07:28 +0300

    Removed code from enemy manager

--- src/game/enemy_manager.cpp
+++ src/game/enemy_manager.cpp
@@ -2,10 +2,4 @@
 
 EnemyManager::EnemyManager() {
     Enemy* enemy = new Enemy(100, 100);
-    timer.start();
-    int ticks = timer.get_ticks();
-    if (ticks > 10 * (1000 / 60)) {
-        enemy->updateVelocity();
-        timer.start();
-    }
 }
