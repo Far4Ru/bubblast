08442df580c2d3fa5d1f9c66841b4d0b14e1f4d6
Far4Ru <faty422@mail.ru>
2024-08-30 23:49:42 +0300

    Updated docs

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -1,6 +1,9 @@
 #include "game/game.hpp"
 
 Game::Game() {
+    // Scene1
+    // Button play_button = new PlayButton();
+    // Scene2
     Background* background = new Background();
     player = new Player();
     sound = new Sound();
@@ -9,8 +12,11 @@ Game::Game() {
     ScreenText* screen_text = new ScreenText();
     bullet_manager = new BulletManager();
     enemy_manager = new EnemyManager();
+    // engine->add->scene(Scene1);
+    // engine->add->scene(Scene2);
 }
 
 void Game::start() {
     engine->start();
+    // engine->start->scene(Scene2);
 }
