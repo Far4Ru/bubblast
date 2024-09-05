b13bb4396d15b5c7c5a2b27598958b2212e8144e
Far4Ru <faty422@mail.ru>
2024-09-06 00:09:39 +0300

    Changed results scene title

--- src/game/components/results/results_title_text.cpp
+++ src/game/components/results/results_title_text.cpp
@@ -4,6 +4,7 @@ ResultsTitleText::ResultsTitleText() {
     text = engine->add->text("WinterCat");
     text->setText("Results");
     text->x = WIDTH / 2 - 70;
+    text->y = 100;
 }
 
 ResultsTitleText::~ResultsTitleText() {
